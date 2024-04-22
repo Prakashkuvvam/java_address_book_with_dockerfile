bb9af134fbb36f4ffb159fda372342031c3283b4
prakash <prakash@mymail.com>
2024-04-22 17:03:08 +0530

    added setup.sh script to install docker and trivy

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y docker.io
+sudo chmod 666 /var/run/docker.sock
+
+sleep 20
+
+sudo apt-get install -y wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install -y trivy
